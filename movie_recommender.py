47b1de456072b7563ecdc97e2c5943355f01d2be
DM <jdmky@outlook.com>
2023-11-02 17:28:30 -0400

    added HTML parser

--- movie_recommender.py
+++ movie_recommender.py
@@ -17,3 +17,14 @@ class ExtractMovies(object):
 #function to make ratings to two decimal places
 def first2(s):
     return s[:4]
+
+url = 'https://www.imdb.com/chart/top/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+movies = soup.select('td.titleColumn')
+links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
+crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
+ratings = [b.attrs.get('data-value') for b in soup.select('td.posterColumn span[name=ir]')]
+years = soup.select('span.secondaryInfo')
+#Temoporary array to store class instances
+_temp_ = []
